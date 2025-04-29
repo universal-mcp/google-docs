39b89fabc7633281cb650d9b1939ef635b94cbcb
Manoj Bajaj <manojbajaj95@gmail.com>
2025-04-29 21:52:09 +0530

    feat: Initial commit from MCP CLI init command

---
+++ src/universal_mcp_google_docs/mcp.py
@@ -0,0 +1,19 @@
+
+from universal_mcp.servers.server import SingleMCPServer
+from universal_mcp.integrations.agentr import AgentRIntegration
+from universal_mcp.stores.store import EnvironmentStore
+
+from universal_mcp_google_docs.app import GoogleDocsApp
+
+env_store = EnvironmentStore()
+integration_instance = AgentRIntegration(name="google-docs", store=env_store)
+app_instance = GoogleDocsApp(integration=integration_instance)
+
+mcp = SingleMCPServer(
+    app_instance=app_instance,
+)
+
+if __name__ == "__main__":
+    mcp.run()
+
+
